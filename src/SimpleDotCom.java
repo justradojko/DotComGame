a05239e4b99720995599807c0bdbd98bdf0d99e8
Radojko Maksimovic <radojkomaksimovic@gmail.com>
2015-09-08 21:29:54 +0200

    Working simple version of DotComGame

--- src/SimpleDotCom.java
+++ src/SimpleDotCom.java
@@ -1,4 +1,24 @@
 
 public class SimpleDotCom {
-
+	int[] locationCells;
+	int numOfHits;
+	
+	public String checkYourself(String guess){
+		for (int cell:locationCells){
+			if (Integer.parseInt(guess) == cell){
+				numOfHits++;
+				
+				if (numOfHits == 3){				
+					return "kill";
+				} else {
+					return "hit";
+				}				
+			}
+		}
+		return "miss";
+	}
+	
+	public void setLocationCells (int[] loc){
+		locationCells = loc;
+	}
 }
--- src/SimpleDotComGame.java
+++ src/SimpleDotComGame.java
@@ -1,13 +1,29 @@
 
 public class SimpleDotComGame {
-	int[] locationCells;
-	int numOfHits;
-	
-	String checkYourself(String guess){
-		return "true";
-	}
-	
-	void setLocationCells (int[] loc){
+	public static void main(String[] args){
+		int numberOfGuesses = 0;
+		GameHelper helper = new GameHelper();
+		
+		SimpleDotCom dot = new SimpleDotCom();
+		int randomNum = (int) (Math.random() * 5);
+		
+		int[] locations = {randomNum, randomNum+1, randomNum+2};
+		
+		dot.setLocationCells(locations);
+		
+		boolean gameActive = true;
+		
+		while ( gameActive == true){
+			String guess = helper.getUserInput("Enter a number");
+			String result = dot.checkYourself(guess);
+			
+			numberOfGuesses++;
+			System.out.println(result);
+			if (result.equals("kill")){
+				gameActive = false;
+				System.out.println("You took " + numberOfGuesses + " guesses");
+			}
+		}
 		
 	}
 }
